f0a6fb27de7b78506ff6b2694954c584c698a8da
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-31 18:00:55 +0700

    class derivedClass turunan dari class baseClass memanggil method perkenalan, error karena methodnya final

--- Program_noOverriding/Program_noOverriding.cpp
+++ Program_noOverriding/Program_noOverriding.cpp
@@ -7,3 +7,10 @@ public:
 		cout << "Hallo saya function dari base class";
 	}
 };
+
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya function dari derived Class";
+	}
+};
