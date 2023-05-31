313b31969a5079c363a452b6d9c74fb5d9272356
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-31 18:35:23 +0700

    class orang dengan 3 member public

---
+++ Program_MultiWaris/Program_MultiWaris.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) : nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+};
