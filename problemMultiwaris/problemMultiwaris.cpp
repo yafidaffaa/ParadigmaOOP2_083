a0b7b7601f353566ce6487fc2995d70be765882a
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-31 11:01:44 +0700

    class orang dengan public akses  yang berisi 2 member

--- problemMultiwaris/problemMultiwaris.cpp
+++ problemMultiwaris/problemMultiwaris.cpp
@@ -1,1 +1,12 @@
- 
+#include <iostream>
+using namespace std;
+
+class orang {
+public: 
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur) {
+		cout << "orang dibuat dengan umur " << umut << "\n" << endl;
+	}
+};
