4104b259a75d8111723072d2e63e7e6e38cc9fe2
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-31 17:24:10 +0700

    class budi turunan dari class pekerja dan pelajar

--- solveMultiwaris/solveMultiwaris.cpp
+++ solveMultiwaris/solveMultiwaris.cpp
@@ -25,3 +25,10 @@ public:
 		cout << "pelajar dibuat\n" << endl;
 	}
 };
+
+class budi : public pekerja, public pelajar {
+public:
+	budi(int pUmur) : pekerja(pUmur), pelajar(pUmur),orang(pUmur) {
+		cout << "Budi Dibuat\n" << endl;
+	}
+};
