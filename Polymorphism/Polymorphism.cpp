c93cf84f5f16c79019db53475db2c10e878953ab
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-31 17:31:21 +0700

    class joko turunan dari class seseorang dan menggunakan method cetak dan pesan dari class seseorang

--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -12,3 +12,13 @@ public:
 	}
 
 };
+
+class joko : public seseorang {
+public:
+	void pesan() {
+		cout << "Pesan dari joko" << endl;
+	}
+	void cetak() {
+		cout << "Tulisan joko" << endl;
+	}
+};
